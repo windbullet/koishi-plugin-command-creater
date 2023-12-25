2dba2949c840276a5c973a0860de9c62aee02aa7
windbullet <2536810643@qq.com>
2023-12-25 11:20:19 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,37 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'command-creater'
 
-export interface Config {}
+export interface Command {
+  name: string
+  cmdAlias: string[]
+  cmdDescription: string
+  reply: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Command: Schema<Command> = Schema.object({
+  name: Schema.string().description("指令名").required(),
+  cmdAlias: Schema.array(Schema.string()).description("指令别名"),
+  cmdDescription: Schema.string().description("指令说明"),
+  reply: Schema.string().description("指令的回复").required(),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export interface Config {
+  commands: Command[]
+}
+
+export const Config: Schema<Config> = Schema.object({
+  commands: Schema.array(Command),
+})
+
+export function apply(ctx: Context, config: Config) {
+  for (const command of config.commands) {
+    ctx.command(command.name, command.cmdDescription ? command.cmdDescription : '')
+      .action(({session}) => {
+        session.send(command.reply)
+      })
+    
+    for (const cmdAlias of command.cmdAlias) {
+      ctx.command(command.name).alias(cmdAlias)
+    }
+  }
 }
